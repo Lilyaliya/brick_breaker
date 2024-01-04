423c73c0f315ee808e858145e688b0fac8af5764
Lilyaliya <lilyaliya@inbox.ru>
2024-01-04 19:45:33 +0300

    feature(brick_breaker): flameGame class changed default anchor position + added play area size & bg

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:brick_breaker/src/brick_breaker.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final game = FlameGame();
+  final game = BrickBreaker();
   runApp(
     GameWidget(game: game)
   );
---
+++ lib/src/app_area.dart
@@ -0,0 +1,3 @@
+// game size
+const gameWidth = 820.0;
+const gameHeight = 1600.0;
